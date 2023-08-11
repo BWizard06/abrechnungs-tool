df6936b0e62d1e2b437ef6b223f1d33fc59ed592
BWizard06 <benbraendle@outlook.com>
2023-08-11 15:36:50 +0200

    authentication for the users

--- pages/api/login.js
+++ pages/api/login.js
@@ -7,7 +7,7 @@ export default function handler(req, res) {
     (username === process.env.USER1_USERNAME && password === process.env.USER1_PASSWORD) ||
     (username === process.env.USER2_USERNAME && password === process.env.USER2_PASSWORD)
   ) {
-    const token = jwt.sign({ username }, process.env.JWT_SECRET, { expiresIn: '1h' });
+    const token = jwt.sign({ username }, process.env.JWT_SECRET, { expiresIn: '7d' });
     res.status(200).json({ token });
   } else {
     res.status(401).json({ error: 'Ungültiger Benutzername oder Passwort' });
--- pages/api/verifyToken.js
+++ pages/api/verifyToken.js
@@ -1,12 +1,13 @@
 import jwt from 'jsonwebtoken';
 
 export default function handler(req, res) {
-  const { token } = req.body;
-
-  try {
-    jwt.verify(token, process.env.JWT_SECRET);
-    res.status(200).json({ valid: true });
-  } catch (err) {
-    res.status(401).json({ valid: false });
+    const { token } = req.body;
+  
+    try {
+      const decoded = jwt.verify(token, process.env.JWT_SECRET);
+      res.status(200).json({ valid: true, username: decoded.username });
+    } catch (err) {
+      res.status(401).json({ valid: false });
+    }
   }
-}
+  
