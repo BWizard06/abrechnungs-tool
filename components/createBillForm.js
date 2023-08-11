ce529dedd37cadbabca775b513de9b99f75af8f9
BWizard06 <benbraendle@outlook.com>
2023-08-11 16:38:24 +0200

    add: create Bill form

--- components/createBillForm.js
+++ components/createBillForm.js
@@ -133,7 +133,7 @@ export default function CreateBillForm() {
                         placeholder="Offerte"
                         value={offerte}
                         onChange={(e) => setOfferte(e.target.value)}
-                        className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                        className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm" 
                     />
                     <input
                         type="date"
